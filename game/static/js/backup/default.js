ff3fb486f34e67da51e5b14bc8c0ad9e3884f943
adam2809 <adam.kule@gmail.com>
2018-11-25 11:10:31 +0000

    Pretty m much works

---
+++ game/static/js/backup/default.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var code = $(".codemirror-textarea")[0];
+
+    var editor = CodeMirror.fromTextArea(code, {
+        
+        lineNumbers : true,
+        mode: "python"
+    
+    });
+});
---
+++ game/static/js/default.js
@@ -0,0 +1,10 @@
+//$(document).ready(function(){
+    var code = document.getElementById('codemirror-textarea')//$("#codemirror-textarea");
+    console.log(code)
+    var editor = CodeMirror.fromTextArea(code, {
+        
+        lineNumbers : true,
+        mode: "python"
+    
+    });
+//});
